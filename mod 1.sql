aa24fb889afe526f7f0e12dc65a4246bb6804ae2
Marcelo <marceloo.cfreitas@gmail.com>
2025-03-11 07:49:28 -0400

    Continuando os estudos

---
+++ mod 1.sql
@@ -0,0 +1,104 @@
+SELECT * FROM v$version;
+
+/*
+DBMS_OUTPUT é um pacote integrado fornecido pela Oracle para exibir informaçõs, debugar e capturar saídas dentro de um programa
+Necessário habilitar com o comando SET SERVEROUTPUT ON;
+*/
+
+SET SERVEROUTPUT ON
+
+--testando o pacote DBMS
+DECLARE
+    mensagem VARCHAR(5) := 'PL-SQL';
+BEGIN
+    dbms_output.put_line(mensagem);
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line('Erro encontrado: ' || sqlerrm);
+END;
+/
+
+--variáveis e tipos de Dados
+DECLARE
+    v_numero NUMBER := 100;
+    v_nome VARCHAR(50) := 'PL-SQL';
+    v_data DATE := SYSDATE;
+    v_valido BOOLEAN := TRUE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Número: ' || v_numero);
+    DBMS_OUTPUT.PUT_LINE('Nome: ' || v_nome);
+    DBMS_OUTPUT.PUT_LINE('Data: ' || TO_CHAR(V_DATA, 'DD/MM/YYYY'));    
+END;
+/
+
+
+--estruturas de controle de fluxo
+--condicionais if/else
+declare
+    v_idade NUMBER := 20;
+begin
+    if v_idade >= 18 then
+        dbms_output.put_line('Maior de idade');
+    else
+        dbms_output.put_line('É menor de idade');
+    end if;    
+end;
+/
+
+
+--case
+DECLARE
+    v_status CHAR(1) := 'A';
+BEGIN
+    CASE V_STATUS
+        WHEN 'A' THEN DBMS_OUTPUT.PUT_LINE('Ativo');
+        WHEN 'I' THEN DBMS_OUTPUT.PUT_LINE('Inativo');
+        ELSE DBMS_OUTPUT.PUT_LINE('Desconhecido');
+    END CASE;
+END;    
+/
+
+--laços de repetição (loop, while, for)
+declare
+    v_contador NUMBER := 1;
+BEGIN
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Contador: ' || v_contador);
+        v_contador := v_contador + 1;
+        EXIT WHEN v_contador > 5;
+    end loop;    
+END;
+/
+
+
+DECLARE
+    v_numero NUMBER := 1;
+BEGIN
+    WHILE v_numero <= 5 LOOP
+        dbms_output.put_line('Número: ' || v_numero);
+        v_numero := v_numero + 1;
+    END LOOP;
+END;
+/
+
+
+BEGIN
+    FOR i IN 1..5 LOOP
+        dbms_output.put_line('Iteração ' || i);
+    END LOOP;
+END;
+/
+
+
+--manipulação de cursores
+
+
+/*
+
+
+
+- Modularização com Procedures e Functions
+- Tratamento de Erros
+- Manipulação de Tabelas e Registros
+*/
+
